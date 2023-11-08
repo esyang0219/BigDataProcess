6ae0caeb82afa6bafdcc2d540124aaf3b32cb034
esyang0219 <80206353+esyang0219@users.noreply.github.com>
2023-11-08 19:03:51 +0900

    Update IMDBStudent20210796.py

--- HW2/IMDBStudent20210796.py
+++ HW2/IMDBStudent20210796.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 import sys
 def count_genre(input_file):
-	genre_count={}
+	movie_count={}
 	with open(input_file, 'r') as f:
 		for line in f:
 			fields=line.split('::')
 			genres=fields[2].split('|')
 			for genre in genres:
 				genre=genre.strip()
-				if genre in genre_count:
-					genre_count[genre] += 1
+				if genre in movie_count:
+					movie_count[genre] += 1
 				else:
-					genre_count[genre] = 1
-	return genre_count
+					movie_count[genre] = 1
+	return movie_count
 
-def write_genre(output_file, genre_count):
+def write_genre(output_file, movie_count):
 	with open(output_file, 'w') as f:
-		for genre, count in genre_count.items():
+		for genre, count in movie_count.items():
 			f.write(f"{genre} {count}\n")
 
 if __name__ == '__main__':
 	input_file=sys.argv[1]
 	output_file=sys.argv[2]
 
-	genre_count = count_genre(input_file)
-	write_genre(output_file, genre_count)
+	movie_count = count_genre(input_file)
+	write_genre(output_file, movie_count)
